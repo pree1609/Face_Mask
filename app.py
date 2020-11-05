c223600c8010f7a89f846bd5e0c85eb29092f846
kritikaberry <kritikaberry@gmail.com>
2020-11-05 20:45:32 +0530

    Added face_detector

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask,render_template,Response,request
-from camera import VideoCamera
+from camera import VideoCamera,VideoCamera2
 import cv2
 import requests
 from werkzeug.utils import secure_filename
@@ -14,10 +14,15 @@ def gen(camera):
 	while(True):
 		frame=camera.get_frame()
 		yield (b'--frame\r\n'
-			b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+			b'Content-Type: image/j6peg\r\n\r\n' + frame + b'\r\n\r\n')
 
 
 @app.route('/video_feed')
 def video_feed():
 	video_stream = VideoCamera()
+	return Response(gen(video_stream),mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/video_feed2')
+def video_feed2():
+	video_stream=VideoCamera2()
 	return Response(gen(video_stream),mimetype='multipart/x-mixed-replace; boundary=frame')
